ed3ef83ba5d42ab5d553b71ed567c230628d397c
Pamela <pam@cs.stanford.edu>
2014-07-29 16:09:18 -0700

    Added some query

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var main = function() {
+	$(document).keypress(function() {
+		//$('.jumbotron').css('background-image', 'http://i.imgur.com/oDEj4bF.jpg');
+	});
+};
+
+$(document).ready(main);
---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+var main=function(){$(document).keypress(function(){})};$(document).ready(main);
